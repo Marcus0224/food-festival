7d4cd333cfc633d54518afea3194a76545721a79
Marcus0224 <marcstlouis04@gmail.com>
2022-05-12 20:48:36 -0400

    bootstrap & jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const { webpack } = require('webpack');
+const webpack = require("webpack")
+const path = require("path");
 
-module.exports = { 
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugun({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-    ],
-    mode: 'development'
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
